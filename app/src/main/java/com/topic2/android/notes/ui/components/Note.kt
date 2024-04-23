78e47858864b080d0fc530458bf21d93ab5e3eb9
meow2meow2meow <167002304+meow2meow2meow@users.noreply.github.com>
2024-04-23 15:44:55 +0300

    lab6

---
+++ app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
@@ -0,0 +1,77 @@
+package com.topic2.android.notes.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.topic2.android.notes.theme.rwGreen
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+
+
+@Composable
+fun Note(){
+    val backgroundShape: Shape = RoundedCornerShape(4.dp)
+    Row(modifier = Modifier
+        .padding(8.dp)
+        .shadow(1.dp, backgroundShape)
+        .fillMaxWidth()
+        .heightIn(min = 64.dp)
+        .background(Color.White, backgroundShape)
+    ) {
+        NoteColor(modifier = Modifier
+            .align(Alignment.CenterVertically)
+            .padding(start = 16.dp, end = 16.dp), rwGreen, 40.dp, 1.dp)
+        Column(modifier = Modifier
+            .weight(1f)
+            .align(Alignment.CenterVertically)
+        ) {
+            Text(
+                text = "Заголовок",
+                color = Color.Black,
+                maxLines = 1,
+                style = TextStyle(
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 16.sp,
+                    letterSpacing = 0.15.sp
+                )
+            )
+            Text(
+                text = "Содержание",
+                color = Color.Black.copy(alpha = 0.75f),
+                maxLines = 1,
+                style = TextStyle(
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 14.sp,
+                    letterSpacing = 0.25.sp
+                )
+            )
+        }
+
+        Checkbox(
+             checked = false, onCheckedChange = {},
+             modifier = Modifier
+                 .padding(start = 8.dp)
+                 .align(Alignment.CenterVertically)
+            )
+
+
+        }
+
+    }
+
+    @Composable
+    fun NotePreview() {
+        Note()
+    }
